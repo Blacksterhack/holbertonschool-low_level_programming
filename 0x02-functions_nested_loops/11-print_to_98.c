23528ab418a998dfc158a0d04aba2e19b9434e68
Blacksterhack <blastertv1001@gmail.com>
2021-03-11 21:57:22 +0000

    low_level:D

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - count for any number
+ * @n: variable to count
+ * Return: result
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 98 ; ++n)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; --n)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
