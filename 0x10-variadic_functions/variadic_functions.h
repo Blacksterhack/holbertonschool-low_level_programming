56ce5fe73683ecebf8873208505992c48844ecb6
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-03-11 17:21:47 -0500

    low-level;D

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void _printchar(va_list args);
-void _printint(va_list args);
-void _printfloat(va_list args);
-void _printstring(va_list args);
-/**
- * struct fo - structure
- * @op: char name for print
- * @f: function to print
- */
-typedef struct fun
-{
-	char *op;
-	void (*f)();
-} fun;
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
