36a1aaf21cdb4c4e13bd4dbeae6cbbf539633929
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-03-12 18:48:24 -0500

    low-level;D

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,78 +1,79 @@
 #include "variadic_functions.h"
 /**
- * _printchar - prints char args
- * @args: args passed to the function
- * Return: void .. by gd
+ * print_char - print characters
+ * @arguments: input arguments
  */
-void _printchar(va_list args)
+void print_char(va_list arguments)
 {
-	printf("%c", va_arg(args, int));
+	printf("%c", va_arg(arguments, int));
 }
 /**
- * _printint - prints int args
- * @args: args passed to the function
- * Return: void
+ * print_int - print integers
+ * @arguments: input arguments
  */
-void _printint(va_list args)
+void print_int(va_list arguments)
 {
-	printf("%d", va_arg(args, int));
+	printf("%d", va_arg(arguments, int));
 }
 /**
- * _printfloat - prints float args
- * @args: args passed to the function
- * Return: void .. by gd
+ * print_float - print floats
+ * @arguments: input arguments
  */
-void _printfloat(va_list args)
+void print_float(va_list arguments)
 {
-	printf("%f", va_arg(args, double));
+	printf("%f", va_arg(arguments, double));
 }
 /**
- * _printstring - prints string args
- * @args: args passed to the function
- * Return: void
+ * print_string - print strings
+ * @arguments: input arguments
  */
-void _printstring(va_list args)
+void print_string(va_list arguments)
 {
-	char *s;
+	char *args = va_arg(arguments, char*);
 
-	s = va_arg(args, char *);
-	printf("%s", s == NULL ? "(nil)" : s);
+	if (args == NULL)
+	{
+		printf("%p", args);
+		return;
+	}
+	printf("%s", args);
 }
 /**
- * print_all - prints all the arguments according to format
- * @format: format
- * Return: void
+ * print_all - prints anything
+ * @format: input string
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int a, b;
-	va_list args;
-	char *s = "";
-
-	fun charformat[] = {
-		{"c", _printchar},
-		{"i", _printint},
-		{"f", _printfloat},
-		{"s", _printstring},
-		{NULL, NULL}
+	simbol_t identifier[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'f', print_float},
+		{'i', print_int}
 	};
-	va_start(args, format);
-	a = 0;
-	while (format && format[a])
+
+	int i = 0, j;
+	char *comma = "";
+
+	va_list arguments;
+
+	va_start(arguments, format);
+
+	while (format && format[i])
 	{
-		b = 0;
-		while (charformat[b].op != NULL)
+		j = 0;
+
+		while (j < 4)
 		{
-			if (format[a] == charformat[b].op[0])
+			if (identifier[j].all == format[i])
 			{
-				printf("%s", s);
-				charformat[b].f(args);
-				s = ", ";
+				printf("%s", comma);
+				identifier[j].func(arguments);
+				comma = ", ";
 			}
-			b++;
+			j++;
 		}
-		a++;
+		i++;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(arguments);
 }
