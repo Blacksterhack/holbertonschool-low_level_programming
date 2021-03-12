f90d65899ffb0ae32bdc3ec4e396769fd884fc13
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-03-12 18:53:35 -0500

    low-level;D

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,79 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_char - print characters
- * @arguments: input arguments
- */
-void print_char(va_list arguments)
-{
-	printf("%c", va_arg(arguments, int));
-}
-/**
- * print_int - print integers
- * @arguments: input arguments
- */
-void print_int(va_list arguments)
-{
-	printf("%d", va_arg(arguments, int));
-}
-/**
- * print_float - print floats
- * @arguments: input arguments
- */
-void print_float(va_list arguments)
-{
-	printf("%f", va_arg(arguments, double));
-}
-/**
- * print_string - print strings
- * @arguments: input arguments
- */
-void print_string(va_list arguments)
-{
-	char *args = va_arg(arguments, char*);
-
-	if (args == NULL)
-	{
-		printf("%p", args);
-		return;
-	}
-	printf("%s", args);
-}
-/**
- * print_all - prints anything
- * @format: input string
- */
+ * print_all - a function that prints anything
+ * @format: format of what will be printed
+ * Return: nothing
+*/
 void print_all(const char * const format, ...)
 {
-	simbol_t identifier[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'f', print_float},
-		{'i', print_int}
-	};
+va_list ap;
+int i = 0, j = 0;
+char *str;
 
-	int i = 0, j;
-	char *comma = "";
-
-	va_list arguments;
-
-	va_start(arguments, format);
-
+va_start(ap, format);
 	while (format && format[i])
 	{
-		j = 0;
-
-		while (j < 4)
+		switch (format[i])
 		{
-			if (identifier[j].all == format[i])
-			{
-				printf("%s", comma);
-				identifier[j].func(arguments);
-				comma = ", ";
-			}
-			j++;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				j = 1;
+				break;
+			case 'i':
+				printf("%i", va_arg(ap, int));
+				j = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				j = 1;
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+				{
+					printf("%p", str);
+					break;
+				}
+				printf("%s", str);
+				j = 1;
+				break;
+			default:
+				j = 0;
 		}
-		i++;
+		if ((j) && (format[i + 1] != '\0'))
+			printf(", ");
+	i++;
 	}
-	printf("\n");
-	va_end(arguments);
+printf("\n");
+va_end(ap);
 }
